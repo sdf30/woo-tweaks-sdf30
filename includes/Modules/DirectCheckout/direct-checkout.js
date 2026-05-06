b180d53e1c93356d8cfc5340e7308fd27840f623
elkwaet <elkwaet@gmail.com>
2026-05-06 12:55:25 +0100

    FIX: Résoudre le clic silencieux du bouton Direct Checkout sur les archives produits (Thème FSE)

--- includes/Modules/DirectCheckout/direct-checkout.js
+++ includes/Modules/DirectCheckout/direct-checkout.js
@@ -1,8 +1,19 @@
 jQuery(function($) {
-    $('.woo-tweaks-fse-direct-checkout').on('click', function(e) {
+    $(document).on('click', '.woo-tweaks-fse-direct-checkout', function(e) {
         e.preventDefault();
         
-        var $form = $('form.cart');
+        var $btn = $(this);
+        
+        // Try to find a related form.cart
+        var $form = $btn.closest('form.cart');
+        
+        if ($form.length === 0) {
+            $form = $btn.closest('.product, .wc-block-grid__product, .type-product, .wp-block-post').find('form.cart');
+        }
+        
+        if ($form.length === 0 && $('form.cart').length === 1) {
+            $form = $('form.cart');
+        }
         
         if ($form.length > 0) {
             // Inject hidden input to flag the direct checkout
@@ -16,6 +27,24 @@ jQuery(function($) {
                 $submitBtn.click();
             } else {
                 $form.submit();
+            }
+        } else {
+            // Fallback for archive pages without form.cart
+            var $addToCartLink = $btn.closest('.product, .wc-block-grid__product, .type-product, .wp-block-post').find('.add_to_cart_button');
+            
+            if ($addToCartLink.length > 0 && $addToCartLink.attr('href')) {
+                var href = $addToCartLink.attr('href');
+                if (href.indexOf('?add-to-cart=') !== -1 || href.indexOf('&add-to-cart=') !== -1) {
+                    var separator = href.indexOf('?') !== -1 ? '&' : '?';
+                    window.location.href = href + separator + 'woo_tweaks_direct_checkout=1';
+                    return;
+                }
+            }
+            
+            // Last resort: fallback to product ID
+            var productId = $btn.data('product_id');
+            if (productId) {
+                window.location.href = '?add-to-cart=' + productId + '&woo_tweaks_direct_checkout=1';
             }
         }
     });
--- includes/Modules/DirectCheckout/render.php
+++ includes/Modules/DirectCheckout/render.php
@@ -2,9 +2,11 @@
 // Render the direct checkout button for FSE
 $label = \get_option('woo_tweaks_direct_checkout_label', \__('Acheter maintenant', 'woo-tweaks-tools'));
 $button_text = \esc_html($label);
+global $product;
+$product_id = $product ? $product->get_id() : 0;
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-    <button type="button" class="button alt woo-tweaks-fse-direct-checkout" style="background: none; color: #007b5f; font-weight: 600; border: solid 1px; padding: 10px;">
+    <button type="button" class="button alt woo-tweaks-fse-direct-checkout" data-product_id="<?php echo esc_attr($product_id); ?>" style="background: none; color: #007b5f; font-weight: 600; border: solid 1px; padding: 10px;">
         <?php echo $button_text; ?>
     </button>
 </div>
