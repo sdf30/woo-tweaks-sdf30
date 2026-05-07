6644a14e5c42d0c744a3184e17bd493de8491af9
elkwaet <elkwaet@gmail.com>
2026-05-07 23:22:46 +0100

    🔖 Release v2.0.1

--- tweak-tools-sdf30.php
+++ tweak-tools-sdf30.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Tweak Tools for WooCommerce
  * Plugin URI:        https://github.com/sdf30/woo-tweaks-sdf30
  * Description:       A collection of performance, UI/UX, and administrative utilities for WooCommerce.
- * Version:           2.0.0
+ * Version:           2.0.1
  * Author:            OPEN-SDF
  * Author URI:        https://open.sdf30.com
  * License:           GPL v2 or later
@@ -40,7 +40,7 @@ final class Plugin
     /**
      * Plugin version.
      */
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.0.1';
 
     /**
      * Constructor.
